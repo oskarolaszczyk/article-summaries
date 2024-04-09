dbd247b0f1c77afffedbfc243712ef47db249a24
oskarolaszczyk <oskarolaszczyk@gmail.com>
2024-04-09 13:19:24 +0200

    config file, used app factory pattern

--- backend/article_summaries/app.py
+++ backend/article_summaries/app.py
@@ -1,9 +1,7 @@
-from flask import Flask
-from apps.core.routes import core_bp
-
-
-app = Flask(__name__)
-app.register_blueprint(core_bp)
+from article_summaries import create_app
 
 if __name__ == "__main__":
+    # with app.app_context():
+    #     db.create_all()
+    app = create_app()
     app.run(host="0.0.0.0", port=8000, debug=True)
