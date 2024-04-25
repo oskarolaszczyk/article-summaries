732f39d8d264558ffd70aeff4328d4307f4a4d0e
oskarolaszczyk <oskarolaszczyk@gmail.com>
2024-04-25 15:59:03 +0200

    added register method

--- frontend/src/components/RegisterPage.js
+++ frontend/src/components/RegisterPage.js
@@ -1,27 +1,52 @@
-import { Button, Container, Form, FormGroup, Nav, Col, Row } from 'react-bootstrap'
-import { NavLink } from 'react-router-dom'
+import axios from 'axios';
+import React, { useContext, useState } from 'react';
+import { Button, Col, Container, Form, FormGroup, Nav, Row } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import { AuthContext } from './AuthContext';
 
 export default function RegisterPage() {
+	const { login } = useContext(AuthContext);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const handleRegister = async () => {
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/auth/register', {
+        username,
+        email,
+        password
+      });
+
+    //   const { access_token, refresh_token, user } = response.data;
+
+    //   login(user, access_token, refresh_token);
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
 	return (
 		<Container fluid className="d-flex justify-content-center">
-			<Form className="px-5 d-flex flex-column my-5 border py-3 justify-content-center">
+			<Form className="px-5 d-flex flex-column my-5 border py-3 justify-content-center" onSubmit={handleRegister}>
 				<h1 className="text-center mb-5">Register</h1>
 				<FormGroup as={Row} className="mb-4">
 					<Form.Label column sm={2} className="me-5">Email:</Form.Label>
 					<Col xs={12} md={8}>
-						<Form.Control placeholder="Enter email" />
+						<Form.Control placeholder="Enter email" type="email" value={email} onChange={e => setEmail(e.target.value)}/>
 					</Col>
 				</FormGroup>
 				<FormGroup as={Row} className="mb-4">
 					<Form.Label column sm={2} className="me-5">Nickname:</Form.Label>
 					<Col xs={12} md={8}>
-						<Form.Control placeholder="Enter nickname" />
+						<Form.Control placeholder="Enter nickname" type="text" value={username} onChange={e => setUsername(e.target.value)}/>
 					</Col>
 				</FormGroup>
 				<FormGroup as={Row} className="mb-4">
 					<Form.Label column sm={2} className="me-5">Password:</Form.Label>
 					<Col xs={12} md={8}>
-						<Form.Control type="password" placeholder="Enter password" />
+						<Form.Control type="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.target.value)}/>
 					</Col>
 				</FormGroup>
 				<Form.Text className="mb-4 text-center">Already have an account?
@@ -29,7 +54,7 @@ export default function RegisterPage() {
 						<span type="button" className="text-primary"> Login</span>
 					</Nav.Link>
 				</Form.Text>
-				<Button variant="primary" className="w-auto mx-auto">Submit</Button>
+				<Button type="submit" variant="primary" className="w-auto mx-auto">Submit</Button>
 			</Form>
 		</Container>
 	)
