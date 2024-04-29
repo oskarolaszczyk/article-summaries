d16d8a66ea66b1ba84f572bc73c71fd154a82a7e
oskarolaszczyk <oskarolaszczyk@gmail.com>
2024-04-30 00:02:17 +0200

    revert config file to render postgresql instance

--- backend/config.py
+++ backend/config.py
@@ -11,7 +11,7 @@ class Config:
     TEMPLATES_FOLDER = "templates"
     TESTING = False
     DEBUG = False
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SECRET_KEY = os.environ.get("SECRET_KEY")
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
@@ -23,9 +23,9 @@ class DevConfig(Config):
     FLASK_ENV = "development"
     TESTING = True
     DEBUG = True
-    DB_USERNAME = os.getenv("POSTGRES_USER")
-    DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-    DB_HOSTNAME = os.getenv("DB_HOSTNAME")
-    DB_NAME = os.getenv("DB_NAME")
-    SQLALCHEMY_DATABASE_URI = (f"postgresql+psycopg2://{DB_USERNAME}"
-                   f":{DB_PASSWORD}@{DB_HOSTNAME}/{DB_NAME}")
+    # DB_USERNAME = os.getenv("POSTGRES_USER")
+    # DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    # DB_HOSTNAME = os.getenv("DB_HOSTNAME")
+    # DB_NAME = os.getenv("DB_NAME")
+    # SQLALCHEMY_DATABASE_URI = (f"postgresql+psycopg2://{DB_USERNAME}"
+    #                f":{DB_PASSWORD}@{DB_HOSTNAME}/{DB_NAME}")
