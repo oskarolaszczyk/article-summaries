ba94f564bf7c0f1869f6d2c0789a2f9593b0bb04
oskarolaszczyk <oskarolaszczyk@gmail.com>
2024-06-02 00:06:29 +0200

    Login and register unit tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -3,16 +3,20 @@ from article_summaries import create_app, db
 from article_summaries.models import User, UserType, Article
 
 
-@pytest.fixture()
+@pytest.fixture(scope="module")
 def app():
     app = create_app()
     app.config.from_object("config.TestConfig")
 
-    yield app
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.session.commit()
+        db.drop_all()
 
 
 @pytest.fixture()
-def client_without_db(app):
+def client(app):
     client = app.test_client()
     ctx = app.test_request_context()
     ctx.push()
@@ -23,25 +27,18 @@ def client_without_db(app):
 
 
 @pytest.fixture()
-def client(client_without_db):
-    db.create_all()
-
-    yield client_without_db
-
-    db.session.commit()
-    db.drop_all()
-
-
-@pytest.fixture()
 def new_user():
-    user = User(username="test", email="test@mail.com", password="test")
+    user = User(username="test", email="test@mail.com", password="password123")
     return user
 
 
 @pytest.fixture()
 def new_user_admin():
     user = User(
-        username="admin", email="admin@mail.com", password="admin", type=UserType.ADMIN
+        username="admin",
+        email="admin@mail.com",
+        password="password123",
+        type=UserType.ADMIN,
     )
     return user
 
@@ -52,3 +49,8 @@ def new_article(new_user):
         user_id=new_user.id, title="Test Article", source_url="https://test.com"
     )
     return article
+
+
+@pytest.fixture()
+def user_data():
+    return {"username": "test", "email": "test@mail.com", "password": "password123"}
--- backend/tests/unit/test_auth.py
+++ backend/tests/unit/test_auth.py
@@ -1,21 +1,89 @@
 import pytest
-from ..conftest import client, new_user
+from ..conftest import client, user_data
 from flask import url_for
 from article_summaries.models import db, User, UserType
 from flask_jwt_extended import create_access_token, create_refresh_token
 
 
-def test_register_user(client):
-    # Correct registration
+def test_register_user_success(client, user_data):
+    response = client.post(url_for("auth_bp.register"), json=user_data)
+    assert response.status_code == 201
+    assert "access_token" in response.json
+    assert "refresh_token" in response.json
+
+
+def test_register_user_missing_data(client):
     response = client.post(
         url_for("auth_bp.register"),
         json={
-            "username": "newuser",
+            "username": "",
             "email": "newuser@example.com",
             "password": "password123",
         },
     )
+    assert response.status_code == 400
+    assert response.json["error"] == "No username or email or password provided."
 
-    assert response.status_code == 201
+
+# def test_register_user_existing_email(client, user_data):
+#     client.post(url_for("auth_bp.register"), json=user_data)
+
+#     response = client.post(
+#         url_for("auth_bp.register"),
+#         json={
+#             "username": "newuser2",
+#             "email": user_data["email"],
+#             "password": "password123",
+#         },
+#     )
+#     assert response.status_code == 400
+#     assert response.json["error"] == "User with given username already exists."
+
+
+def test_register_user_existing_username(client, user_data):
+    response = client.post(
+        url_for("auth_bp.register"),
+        json={
+            "username": user_data["username"],
+            "email": "newemail@example.com",
+            "password": "password123",
+        },
+    )
+    assert response.status_code == 400
+    assert response.json["error"] == "User with given username already exists."
+
+
+def test_login_user_success(client, user_data):
+    response = client.post(
+        url_for("auth_bp.login"),
+        json={"username": user_data["username"], "password": user_data["password"]},
+    )
+    assert response.status_code == 200
     assert "access_token" in response.json
     assert "refresh_token" in response.json
+
+
+def test_login_user_wrong_password(client, user_data):
+    response = client.post(
+        url_for("auth_bp.login"),
+        json={"username": user_data["username"], "password": "wrongpassword"},
+    )
+    assert response.status_code == 401
+    assert response.json["error"] == "Please check your login details and try again."
+
+
+def test_login_user_nonexistent_user(client):
+    response = client.post(
+        url_for("auth_bp.login"),
+        json={"username": "nonexistentuser", "password": "password"},
+    )
+    assert response.status_code == 404
+    assert response.json["error"] == "User with given credentials does not exist."
+
+
+def test_login_user_missing_data(client):
+    response = client.post(
+        url_for("auth_bp.login"), json={"username": "", "password": "password"}
+    )
+    assert response.status_code == 400
+    assert response.json["error"] == "No username or email or password provided."
