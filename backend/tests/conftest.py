df9bdcca817d2a69ad552900aa53bb3c5a84f00f
oskarolaszczyk <oskarolaszczyk@gmail.com>
2024-06-01 19:19:35 +0200

    Add init unit test - article and summary

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from article_summaries import User, UserType
+from article_summaries.models import User, UserType, Article
 
 
 @pytest.fixture()
@@ -14,3 +14,11 @@ def new_user_admin():
         username="admin", email="admin@mail.com", password="admin", type=UserType.ADMIN
     )
     return user
+
+
+@pytest.fixture()
+def new_article(new_user):
+    article = Article(
+        user_id=new_user.id, title="Test Article", source_url="https://test.com"
+    )
+    return article
--- backend/tests/unit/test_models.py
+++ backend/tests/unit/test_models.py
@@ -1,4 +1,4 @@
-from article_summaries.models import UserType
+from article_summaries.models import UserType, Summary, ModelType
 from ..conftest import new_user, new_user_admin
 
 
@@ -14,3 +14,33 @@ def test_new_user_admin(new_user_admin):
     assert new_user_admin.email == "admin@mail.com"
     assert new_user_admin.password != "admin"  # hashed password
     assert new_user_admin.type == UserType.ADMIN
+
+
+def test_new_article(new_user, new_article):
+    assert new_article.user_id == new_user.id
+    assert new_article.title == "Test Article"
+    assert new_article.source_url == "https://test.com"
+
+
+def test_new_summary_our_model(new_article):
+    summary = Summary(
+        article_id=new_article.id,
+        content="Test summary",
+        model_type=ModelType.OUR_MODEL,
+    )
+
+    assert summary.article_id == new_article.id
+    assert summary.content == "Test summary"
+    assert summary.model_type == ModelType.OUR_MODEL
+
+
+def test_new_summary_meaningcloud(new_article):
+    summary = Summary(
+        article_id=new_article.id,
+        content="Test summary",
+        model_type=ModelType.MEANINGCLOUD,
+    )
+
+    assert summary.article_id == new_article.id
+    assert summary.content == "Test summary"
+    assert summary.model_type == ModelType.MEANINGCLOUD
