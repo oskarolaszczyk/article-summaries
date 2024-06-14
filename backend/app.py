57a8bd592d1b2fa2de3f742b5db65a11c51940f6
oskarolaszczyk <oskarolaszczyk@gmail.com>
2024-06-14 13:40:23 +0200

    Commented app context with droping tables db

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from article_summaries.models import db
 
 if __name__ == "__main__":
     app = create_app()
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
+    # with app.app_context():
+    #     db.drop_all()
+    #     db.create_all()
     app.run(host="0.0.0.0", port=5000, debug=True)
